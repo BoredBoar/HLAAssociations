99e85a83ba738e7665cb80ca185e1d04fd46ad10
Chris Williams <christopher.lee.williams@gmail.com>
2024-12-13 16:51:18 -0600

    Starting to add DR-DQ associations

---
+++ parse_csv3.js
@@ -0,0 +1,13 @@
+import { parse } from 'csv-parse/sync';
+import * as fs from 'fs'
+
+const data = fs.readFileSync('./DR-DQ.txt',
+    { encoding: 'utf8', flag: 'r' });
+
+const output = parse(data, {
+    delimiter: ['\t'],
+    trim: true,
+    columns: true,
+    })
+
+    console.log(JSON.stringify(output))
